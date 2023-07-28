eb06806a53e4662fbfdb687b9456487c127cf286
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-07-28 10:02:20 +0100

    Upgrade

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -11,6 +11,8 @@ const Homepage = () => {
     const [filteredItems, setFilteredItems] = useState([])
     const [sortCriteria, setSortCriteria] = useState('')
     const [filteredCriteria, setFilteredCriteria] = useState('')
+    const [collectionDropDown, setCollectionDropDown] = useState(false)
+    const [sortDropDown, setSortDropDown] = useState(false)
 
     
     const fetchData = async() => {
@@ -32,21 +34,26 @@ const Homepage = () => {
             const filtered = products.filter((product) => product.category === criterion)
             setFilteredItems(filtered)
             setFilteredCriteria('Men')
+            setCollectionDropDown(false)
         } else if (criterion === `jewelery`){
             const filtered = products.filter((product) => product.category === criterion)
             setFilteredItems(filtered)
             setFilteredCriteria('Jeweleries')
+            setCollectionDropDown(false)
         } else if (criterion === `women's clothing`){
             const filtered = products.filter((product) => product.category === criterion)
             setFilteredItems(filtered)
             setFilteredCriteria('Women')
+            setCollectionDropDown(false)
         } else if (criterion === `electronics`){
             const filtered = products.filter((product) => product.category === criterion)
             setFilteredItems(filtered)
             setFilteredCriteria('Electronics')
+            setCollectionDropDown(false)
         } else if(criterion === 'All'){
             setFilteredItems(products)
             setFilteredCriteria('All')
+            setCollectionDropDown(false)
         }
     }
 
@@ -55,19 +62,31 @@ const Homepage = () => {
         const sorted = [...filteredItems].sort((a, b) => a.price - b.price)
         setFilteredItems(sorted)
         setSortCriteria('Price: Low to High')
+        setSortDropDown(false)
     } else if (criterion === 'descPrice'){
         const sorted = [...filteredItems].sort((a, b) => b.price - a.price)
         setFilteredItems(sorted)
         setSortCriteria('Price: High to Low')
+        setSortDropDown(false)
     } else if (criterion === 'trending'){
         const sorted = [...filteredItems].sort((a, b) => a.rating.count - b.rating.count)
         setFilteredItems(sorted)
         setSortCriteria('Trending')
+        setSortDropDown(false)
     } else if (criterion === 'relevance'){
         const sorted = [...filteredItems].sort((a, b) => a.rating.rate - b.rating.rate)
         setFilteredItems(sorted)
         setSortCriteria('Relevance')
+        setSortDropDown(false)
     }
+   }
+
+   const handleCollectionDropDown = () => {
+    setCollectionDropDown(!collectionDropDown)
+   }
+
+   const handleSortDropDown = () => {
+    setSortDropDown(!sortDropDown)
    }
 
   return (
@@ -76,19 +95,19 @@ const Homepage = () => {
 
         <section>
 
-            <div className='order-1 '>
+            <div className='order-1'>
                 <p className='text-[#5d5d5d] lg:block hidden'>Collections</p>
-                <div className='lg:hidden flex justify-between rounded-md w-full border p-2 text-white'>
+                <div className='drop-down'>
                     <div>{filteredCriteria}</div>
-                    <img src="/Image/arrow-down.svg" alt="arrow" className='w-[5vw]' />
+                    <img src="/Image/arrow-down.svg" alt="arrow" className='w-[5vw]' onClick={handleCollectionDropDown} />
                 </div>
-                <ul className='collection'>
-                    <li><a href='#' onClick={() => handleFilter('All')}>All</a></li>
-                    <li><a href='#' onClick={() =>handleFilter(`men's clothing`)}>Men</a></li>
-                    <li><a href='#' onClick={() => handleFilter(`women's clothing`)}>Women</a></li>
-                    <li><a href='#' onClick={() => handleFilter(`electronics`)}>Electronics</a></li>
-                    <li><a href='#' onClick={() => handleFilter(`jewelery`)}>Jeweleries</a></li>
-                </ul>
+                <ul className={`collection ${collectionDropDown ? 'dropped' : ''}`}>
+                        <li><a href='#' onClick={() => handleFilter('All')}>All</a></li>
+                        <li><a href='#' onClick={() =>handleFilter(`men's clothing`)}>Men</a></li>
+                        <li><a href='#' onClick={() => handleFilter(`women's clothing`)}>Women</a></li>
+                        <li><a href='#' onClick={() => handleFilter(`electronics`)}>Electronics</a></li>
+                        <li><a href='#' onClick={() => handleFilter(`jewelery`)}>Jeweleries</a></li>
+                    </ul>
             </div>
             
             <main className='productWrapper lg:order-2 order-3'>
@@ -109,17 +128,17 @@ const Homepage = () => {
             <div className='lg:order-3 order-2'>
                 <p className='text-[#5d5d5d] lg:block hidden'>Sort by</p>
 
-                <div className='lg:hidden flex justify-between rounded-md w-full border p-2 text-white'>
+                <div className='drop-down'>
                     <div>{sortCriteria}</div>
-                    <img src="/Image/arrow-down.svg" alt="arrow" className='w-[5vw]' />
+                    <img src="/Image/arrow-down.svg" alt="arrow" className='w-[5vw]' onClick={handleSortDropDown} />
                 </div>
 
-                <ul className='sort'>
-                    <li><a href='#' onClick={() => handleSort('relevance')}>Relevance</a></li>
-                    <li><a href='#' onClick={() => handleSort('trending')}>Trending</a></li>
-                    <li><a href='#' onClick={() => handleSort('ascPrice')}>Price: Low to High</a></li>
-                    <li><a href='#' onClick={() => handleSort('descPrice')}>Price: High to Low</a></li>
-                </ul>
+                <ul className={`sort ${sortDropDown ? 'dropped': ''}`}>
+                        <li><a href='#' onClick={() => handleSort('relevance')}>Relevance</a></li>
+                        <li><a href='#' onClick={() => handleSort('trending')}>Trending</a></li>
+                        <li><a href='#' onClick={() => handleSort('ascPrice')}>Price: Low to High</a></li>
+                        <li><a href='#' onClick={() => handleSort('descPrice')}>Price: High to Low</a></li>
+                    </ul>
             </div>
         </section>
     </div>
