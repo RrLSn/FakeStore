b52ca69e5d823c813b0c08efacd69c50c44ae5fe
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-07-28 12:51:53 +0100

    Upgrade

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavBar = () => {
   return (
     <nav className='w-full flex items-center text-white justify-between'>
-            <h1 className='lg:text-3xl text-2xl font-bold'>FakeStore.<sub className='italic'>ng</sub></h1>
+            <a href="/"><h1 className='lg:text-3xl text-2xl font-bold'>FakeStore.<sub className='italic'>ng</sub></h1></a>
 
             <div className='lg:flex  hidden w-[25rem] justify-between rounded-md border border-[#5d5d5d] p-2'>
                 <input type="text" placeholder='Search for products' className='bg-transparent focus:outline-none w-full' />
